2adf54b06d6621601b49b8333ecbd66987b74bf1
tom.wall@bakertilly.com <tom.wall@bakertilly.com>
2023-02-21 17:10:03 +0000

    Update some of the Advanced Jinja exercises

---
+++ macros/grant_select.sql
@@ -0,0 +1,12 @@
+{% macro grant_select(schema=target.schema, role=target.role)%}
+/*This is specifically designed for Snowflake... will not work in Redshift because it doesn't use roles*/
+    {% set query %}
+        grant usage on schema {{schema}} to role {{role}};
+        grant select on all tables in schema {{schema}} to role {{role}};
+        grant select on all views in schema {{schema}} to role {{role}};
+    {% endset %}
+
+    {{log('Granting select on all tables and views in schema ' ~ schema ~ ' to role ' ~ role, info=True)}}
+    {% do run_query(query)%}
+    {{log('Privileges granted!', info=True)}}
+{% endmacro %}
